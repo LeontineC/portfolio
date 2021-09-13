085bce3761af6e6e11388a4971259bccbb6fdd8f
Leontine Claassen <leon.claassen00@gmail.com>
2021-09-13 17:05:39 +0200

    image

--- src/Components/Particles.js
+++ src/Components/Particles.js
@@ -6,7 +6,7 @@ const Particles = () => {
     return (
         <>
            
-             <div class="particles">
+             <div class="particles" style={{backgroundImage: `url('https://images.unsplash.com/photo-1597839219216-a773cb2473e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')`}}>
         <span ></span>
         <span ></span>
         <span ></span>
